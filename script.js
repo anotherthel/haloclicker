2ff40ae527ec36e8b700433c95fa4a03fa976873
Thel 'Vadam likes thanksgiving <jason94462@gmail.com>
2020-03-01 16:54:32 -0800

    Fix reset function

--- script.js
+++ script.js
@@ -621,79 +621,11 @@ function load(){
 // change
 function reset()
 {
-     ff={
-    money: 20,
-    income: 0,
-    cpc: 1,
-    marines: [0, 5, 1, 1, 10, 15],
-    odsts: [0, 30, 3, 2, 25, 30],
-    spartans: [0, 1000, 25, 5, 100, 150],
-    bought: false,
-    housing: 0,
-    troops: 0,
-
-  camps:{
-    housing: 1,
-    cost: 10
-  },
-  barracks:{
-    housing: 3,
-    cost: 25
-  },
-  bases: {
-    housing: 20,
-    cost: 250
-  },
-
-    wins: 0,
-    losses: 0,
-    ties: 0,
-    chance: null,
-    basic_factory: [0, 75, 1],// income: amount, cost, $/sec
-    tier2_factory: [0, 250, 2],
-    tier3_factory: [0, 1000, 5],
-    shipments: [0, 5000, 10],
-
-    tick: 1000,
-    _cost: 1000,
-    font: 'Halo',
-    times: 0,
-
-    /*metal: 0,
-    ore: 0,
-    miners: 0,
-    metalW: 0,
-
-    metalHousing: 0,
-    metalReq: 10,
-    oreReq: 15,
-    mineHousing: 0
-
-    metalWorker:{
-      cost: 10,
-      amount: 0
-    }*/
-
-    ore: {
-      'amount': 0,
-      'workers': 0,
-      'fact': 0,
-      'cpc': 0
-    },
-    steel: {
-      'amount': 0,
-      'workers': 0,
-      'fact': 0,
-      'cpc': 0
-    }
-
-}
-max=100;
-req=25000;
-
-    updateBattleStats();
-    updateview();
-    save();
+     window.localStorage.clear();
+     location.reload();
+     updateview();
+     updateBattleStats();
+     save_();
 }
 
 
