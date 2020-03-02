d3f08c13f314a1124e6ffd7e7517f6ee304580c6
Thel 'Vadam likes thanksgiving <jason94462@gmail.com>
2020-03-02 07:19:28 -0800

    fix JSON stuff

--- script.js
+++ script.js
@@ -9,41 +9,41 @@ var test=0;
 
 let autosaving=true;
 
-var stuff={
-	"money": 20,
-	"income": 0,
-	"cpc": 1,
-	"marines": [0, 5, 1, 1, 10, 15],
-	"odsts": [0, 30, 3, 2, 25, 30],
-	"spartans": [0, 1000, 25, 5, 100, 150],
-	"bought": false,
-	"housing": 0,
-	"troops": 0,
-	"camps": {
-		"housing": 1,
-		"cost": 10
+var ff={
+	money: 20,
+	income: 0,
+	cpc: 1,
+	marines: [0, 5, 1, 1, 10, 15],
+	odsts: [0, 30, 3, 2, 25, 30],
+	spartans: [0, 1000, 25, 5, 100, 150],
+	bought: false,
+	housing: 0,
+	troops: 0,
+	camps: {
+		"housing: 1,
+		"cost: 10
 	},
-	"barracks": {
+	barracks: {
 		"housing": 3,
 		"cost": 25
 	},
-	"bases": {
+	bases: {
 		"housing": 20,
 		"cost": 250
 	},
-	"wins": 0,
-	"losses": 0,
-	"ties": 0,
-	"chance": 0,
-	"basic_factory": [0, 75, 1],
-	"tier2_factory": [0, 250, 2],
-	"tier3_factory": [0, 1000, 5],
-	"shipments": [0, 5000, 10],
-	"tick": 1000,
-	"_cost": 1000,
-	"font": "Halo",
-	"times": 0,
-	"ore": {
+	wins: 0,
+	losses: 0,
+	ties: 0,
+	chance: 0,
+	basic_factory: [0, 75, 1],
+	tier2_factory: [0, 250, 2],
+	tier3_factory: [0, 1000, 5],
+	shipments: [0, 5000, 10],
+	tick: 1000,
+	_cost: 1000,
+	font: "Halo",
+	times: 0,
+	ore: {
 		"amount": 0,
 		"workers": 0,
 		"fact": 0,
@@ -51,7 +51,7 @@ var stuff={
 		"cpc": 0,
 		"max": 50
 	},
-	"steel": {
+	steel: {
 		"amount": 0,
 		"workers": 0,
 		"fact": 0,
@@ -59,10 +59,10 @@ var stuff={
 		"cpc": 0,
 		"max": 50
 	},
-	"cost": 0
+	cost: 0
 }
 
-var ff=JSON.parse(stuff);
+//var ff=JSON.parse(stuff);
 
 var max=100;
 var req=25000;
