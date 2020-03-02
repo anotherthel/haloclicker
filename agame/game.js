204727cac666083890a2f79f6ed1991d52b38d37
Thel 'Vadam likes thanksgiving <jason94462@gmail.com>
2020-03-02 12:32:50 -0800

    add save() function

--- agame/game.js
+++ agame/game.js
@@ -1,1 +1,15 @@
-var player={}
+var player={
+  protons: 0,
+  electrons: 0,
+  neutrons: 0
+
+}
+
+
+
+function save(){
+  window.localStorage.clear();
+  var _player=JSON.stringify(player);
+  localStorage.setItem('_player', _player);
+  $.notify('Game saved.', "info");
+}
