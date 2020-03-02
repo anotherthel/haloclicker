b4488d25d3beedc11481e4607711b83e1410183e
Thel 'Vadam likes thanksgiving <jason94462@gmail.com>
2020-03-02 15:16:25 -0800

    commit message for testing

--- agame/game.js
+++ agame/game.js
@@ -1,10 +1,46 @@
 var player={
-  protons: 0,
-  electrons: 0,
-  neutrons: 0
+  protons: {
+    'amount': 0,
+    'income': 1
+  },
+  electrons: {
+    'amount': 0,
+    'income': 0
+  },
+  neutrons: {
+    'amount': 0,
+    'income': 0
+  }
 
 }
 
+
+
+
+function showval(id, content){
+  return document.getElementById(id).innerHTML=parseInt(content.toFixed(2)).toLocaleString('en-US');
+}
+function c(id){
+  return document.getElementById(id);
+}
+
+setInterval(function(){// function for income
+  player.protons.amount+=player.protons.income/100;
+  player.electrons.amount+=player.electrons.income/100;
+  showval('protons', player.protons.amount);
+  showval('electrons', player.electrons.amount);
+}, 10)
+
+
+
+
+function gainResource(res){
+  res.amount+=1;
+}
+
+
+c('gainProton').onclick=gainResource(player.protons);
+c('gainElectron').onclick=gainResource(player.protons);
 
 
 function save(){
