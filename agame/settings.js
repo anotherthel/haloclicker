18ead251bacbc1b058f4e5dc161d3baf80d2ba99
Thel 'Vadam likes thanksgiving <jason94462@gmail.com>
2020-03-02 18:22:34 -0800

    make nav bar active

--- agame/settings.js
+++ agame/settings.js
@@ -15,6 +15,10 @@ class Settings{
     this.interval=x;
   }
 
-  
+  tabs(show, h1, h2){
+    document.getElementById(show).style.display="block";
+    document.getElementById(h1).style.display="none";
+    document.getElementById(h2).style.display="none";
+  }
 }
 s=new Settings(true, 30000)
